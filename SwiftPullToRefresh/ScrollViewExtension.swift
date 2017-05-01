3232fc7fda97343a7b263f5f06bd6154c16ac4ba
Leo Zhou <wxg.bridgeq@gmail.com>
2017-05-02 00:18:15 +0800

    add comments for public methods

--- SwiftPullToRefresh/ScrollViewExtension.swift
+++ SwiftPullToRefresh/ScrollViewExtension.swift
@@ -32,12 +32,28 @@ public extension UIScrollView {
         }
     }
     
+    /// Indicator style header
+    ///
+    /// - Parameters:
+    ///   - color: indicator color, default is (R:0, G:0, B:0, A:0.8)
+    ///   - height: refresh view height and also the trigger requirement, default is 60
+    ///   - action: refresh action
     public func spr_addIndicatorHeader(color: UIColor = Default.color,
                                        height: CGFloat = Default.short,
                                        action: @escaping () -> Void) {
         spr_refreshHeaderView = IndicatorHeaderView(color: color, height: height, action: action)
     }
     
+    /// Text style header
+    ///
+    /// - Parameters:
+    ///   - loadingText: text display for refreshing, default is 'Loading...'
+    ///   - pullingText: text display for dragging when don't reach the trigger, default is 'Pull down to refresh'
+    ///   - releaseText: text display for dragging when reach the trigger, default is 'Release to refresh'
+    ///   - font: text font, default is System 14.0
+    ///   - color: color for indicator and text, default is (R:0, G:0, B:0, A:0.8)
+    ///   - height: refresh view height and also the trigger requirement, default is 60
+    ///   - action: refresh action
     public func spr_addTextHeader(loadingText: String = Default.loadingText,
                                   pullingText: String = Default.pullingText,
                                   releaseText: String = Default.releaseText,
@@ -48,6 +64,13 @@ public extension UIScrollView {
         spr_refreshHeaderView = TextHeaderView(loadingText: loadingText, pullingText: pullingText, releaseText: releaseText, font: font, color: color, height: height, action: action)
     }
     
+    /// GIF style header
+    ///
+    /// - Parameters:
+    ///   - data: data for the GIF file
+    ///   - isBig: whether the GIF is displayed with full width
+    ///   - height: refresh view height and also the trigger requirement
+    ///   - action: refresh action
     public func spr_addGIFHeader(data: Data,
                                  isBig: Bool,
                                  height: CGFloat,
@@ -55,6 +78,17 @@ public extension UIScrollView {
         spr_refreshHeaderView = GIFHeaderView(data: data, isBig: isBig, height: height, action: action)
     }
     
+    /// GIF + Text style header
+    ///
+    /// - Parameters:
+    ///   - data: data for the GIF file
+    ///   - loadingText: text display for refreshing, default is 'Loading...'
+    ///   - pullingText: text display for dragging when don't reach the trigger, default is 'Pull down to refresh'
+    ///   - releaseText: text display for dragging when reach the trigger, default is 'Release to refresh'
+    ///   - font: text font, default is System 14.0
+    ///   - color: text color, default is (R:0, G:0, B:0, A:0.8)
+    ///   - height: refresh view height and also the trigger requirement, default is 60
+    ///   - action: refresh action
     public func spr_addGIFTextHeader(data: Data,
                                      loadingText: String = Default.loadingText,
                                      pullingText: String = Default.pullingText,
@@ -66,17 +100,36 @@ public extension UIScrollView {
         spr_refreshHeaderView = GIFTextHeaderView(data: data, loadingText: loadingText, pullingText: pullingText, releaseText: releaseText, font: font, color: color, height: height, action: action)
     }
     
-    public func spr_addSuperCatHeader(height: CGFloat = Default.high,
-                                      action: @escaping () -> Void) {
-        spr_refreshHeaderView = SuperCatHeaderView(height: height, action: action)
+    /// SuperCat style header, inspired by RayWenderlich
+    /// https://videos.raywenderlich.com/courses/68-scroll-view-school/lessons/18
+    ///
+    /// - Parameter action: refresh action
+    public func spr_addSuperCatHeader(action: @escaping () -> Void) {
+        spr_refreshHeaderView = SuperCatHeaderView(height: 120, action: action)
     }
     
+    /// Indicator style footer
+    ///
+    /// - Parameters:
+    ///   - color: indicator color, default is (R:0, G:0, B:0, A:0.8)
+    ///   - height: refresh view height and also the trigger requirement, default is 60
+    ///   - action: refresh action
     public func spr_addIndicatorFooter(color: UIColor = Default.color,
                                        height: CGFloat = Default.short,
                                        action: @escaping () -> Void) {
         spr_refreshFooterView = IndicatorFooterView(color: color, height: height, action: action)
     }
     
+    /// Text style footer
+    ///
+    /// - Parameters:
+    ///   - loadingText: text display for refreshing, default is 'Loading...'
+    ///   - pullingText: text display for dragging when don't reach the trigger, default is 'Pull up to load more'
+    ///   - releaseText: text display for dragging when reach the trigger, default is 'Release to load more'
+    ///   - font: text font, default is System 14.0
+    ///   - color: color for indicator and text, default is (R:0, G:0, B:0, A:0.8)
+    ///   - height: refresh view height and also the trigger requirement, default is 60
+    ///   - action: refresh action
     public func spr_addTextFooter(loadingText: String = Default.loadingText,
                                   pullingText: String = Default.pullingFooterText,
                                   releaseText: String = Default.releaseFooterText,
@@ -87,18 +140,28 @@ public extension UIScrollView {
         spr_refreshFooterView = TextFooterView(loadingText: loadingText, pullingText: pullingText, releaseText: releaseText, font: font, color: color, height: height, action: action)
     }
     
+    /// Custom style header
+    /// Subclasses need to implement 'updateRefreshState(_:)' and 'updateProgress(_:)' methods
+    ///
+    /// - Parameter headerView: your custom header subclass
     public func spr_addCustomHeader(headerView: RefreshHeaderView) {
         spr_refreshHeaderView = headerView
     }
     
+    /// Custom style footer
+    /// Subclasses need to implement 'updateRefreshState(_:)' and 'updateProgress(_:)' methods
+    ///
+    /// - Parameter footerView: your custom footer subclass
     public func spr_addCustomFooter(footerView: RefreshFooterView) {
         spr_refreshFooterView = footerView
     }
     
+    /// begin refreshing
     public func spr_beginRefreshing() {
         spr_refreshHeaderView?.beginRefreshing()
     }
     
+    /// end refreshing
     public func spr_endRefreshing() {
         spr_refreshHeaderView?.endRefreshing()
         spr_refreshFooterView?.endRefreshing()
@@ -115,7 +178,6 @@ struct Default {
     static let releaseText = "Release to refresh"
     static let pullingFooterText = "Pull up to load more"
     static let releaseFooterText = "Release to load more"
-    
     static let high: CGFloat = 120
     static let short: CGFloat = 60
 }
