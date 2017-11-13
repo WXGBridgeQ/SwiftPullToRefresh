990e6bfbfa800bfb0843abfa572de0655a6b30de
Simone Valente <simone.valente@accenture.com>
2017-11-13 12:39:49 +0100

    Swift 4 error fixed

--- SwiftPullToRefresh/ScrollViewExtension.swift
+++ SwiftPullToRefresh/ScrollViewExtension.swift
@@ -21,7 +21,7 @@ public extension UIScrollView {
             newValue.map { insertSubview($0, at: 0) }
         }
     }
-
+    
     private var spr_refreshFooter: RefreshView? {
         get {
             return objc_getAssociatedObject(self, &refreshFooterKey) as? RefreshView
@@ -31,17 +31,17 @@ public extension UIScrollView {
             newValue.map { insertSubview($0, at: 0) }
         }
     }
-
+    
     /// Indicator header
     ///
     /// - Parameters:
     ///   - height: refresh view height and also the trigger requirement, default is 60
     ///   - action: refresh action
-    public func spr_setIndicatorHeader(height: CGFloat = Default.short,
+    public func spr_setIndicatorHeader(height: CGFloat = DefaultSPTR.short,
                                        action: @escaping () -> Void) {
         spr_refreshHeader = IndicatorHeader(height: height, action: action)
     }
-
+    
     /// Text header
     ///
     /// - Parameters:
@@ -50,15 +50,15 @@ public extension UIScrollView {
     ///   - releaseText: text display for dragging when reach the trigger, default is 'Release to refresh'
     ///   - height: refresh view height and also the trigger requirement, default is 60
     ///   - action: refresh action
-    public func spr_setTextHeader(loadingText: String = Default.loadingText,
-                                  pullingText: String = Default.pullingText,
-                                  releaseText: String = Default.releaseText,
-                                  height: CGFloat = Default.short,
+    public func spr_setTextHeader(loadingText: String = DefaultSPTR.loadingText,
+                                  pullingText: String = DefaultSPTR.pullingText,
+                                  releaseText: String = DefaultSPTR.releaseText,
+                                  height: CGFloat = DefaultSPTR.short,
                                   action: @escaping () -> Void) {
         let textItem = TextItem(loadingText: loadingText, pullingText: pullingText, releaseText: releaseText)
         spr_refreshHeader = TextHeader(textItem: textItem, height: height, action: action)
     }
-
+    
     /// GIF header
     ///
     /// - Parameters:
@@ -72,7 +72,7 @@ public extension UIScrollView {
                                  action: @escaping () -> Void) {
         spr_refreshHeader = GIFHeader(data: data, isBig: isBig, height: height, action: action)
     }
-
+    
     /// GIF + Text header
     ///
     /// - Parameters:
@@ -83,25 +83,25 @@ public extension UIScrollView {
     ///   - height: refresh view height and also the trigger requirement, default is 60
     ///   - action: refresh action
     public func spr_setGIFTextHeader(data: Data,
-                                     loadingText: String = Default.loadingText,
-                                     pullingText: String = Default.pullingText,
-                                     releaseText: String = Default.releaseText,
-                                     height: CGFloat = Default.short,
+                                     loadingText: String = DefaultSPTR.loadingText,
+                                     pullingText: String = DefaultSPTR.pullingText,
+                                     releaseText: String = DefaultSPTR.releaseText,
+                                     height: CGFloat = DefaultSPTR.short,
                                      action: @escaping () -> Void) {
         let textItem = TextItem(loadingText: loadingText, pullingText: pullingText, releaseText: releaseText)
         spr_refreshHeader = GIFTextHeader(data: data, textItem: textItem, height: height, action: action)
     }
-
+    
     /// Indicator footer
     ///
     /// - Parameters:
     ///   - height: refresh view height and also the trigger requirement, default is 60
     ///   - action: refresh action
-    public func spr_setIndicatorFooter(height: CGFloat = Default.short,
+    public func spr_setIndicatorFooter(height: CGFloat = DefaultSPTR.short,
                                        action: @escaping () -> Void) {
         spr_refreshFooter = IndicatorFooter(height: height, action: action)
     }
-
+    
     /// Text footer
     ///
     /// - Parameters:
@@ -110,38 +110,38 @@ public extension UIScrollView {
     ///   - releaseText: text display for dragging when reach the trigger, default is 'Release to load more'
     ///   - height: refresh view height and also the trigger requirement, default is 60
     ///   - action: refresh action
-    public func spr_setTextFooter(loadingText: String = Default.loadingText,
-                                  pullingText: String = Default.pullingFooterText,
-                                  releaseText: String = Default.releaseFooterText,
-                                  height: CGFloat = Default.short,
+    public func spr_setTextFooter(loadingText: String = DefaultSPTR.loadingText,
+                                  pullingText: String = DefaultSPTR.pullingFooterText,
+                                  releaseText: String = DefaultSPTR.releaseFooterText,
+                                  height: CGFloat = DefaultSPTR.short,
                                   action: @escaping () -> Void) {
         let textItem = TextItem(loadingText: loadingText, pullingText: pullingText, releaseText: releaseText)
         spr_refreshFooter = TextFooter(textItem: textItem, height: height, action: action)
     }
-
+    
     /// Indicator auto refresh footer (auto triggered when scroll down to the bottom of the content)
     ///
     /// - Parameters:
     ///   - height: refresh view height and also the trigger requirement, default is 60
     ///   - action: refresh action
-    public func spr_setIndicatorAutoFooter(height: CGFloat = Default.short,
+    public func spr_setIndicatorAutoFooter(height: CGFloat = DefaultSPTR.short,
                                            action: @escaping () -> Void) {
         spr_refreshFooter = IndicatorFooter(height: height, isAuto: true, action: action)
     }
-
+    
     /// Text auto refresh footer (auto triggered when scroll down to the bottom of the content)
     ///
     /// - Parameters:
     ///   - loadingText: text display for refreshing, default is 'Loading...'
     ///   - height: refresh view height and also the trigger requirement, default is 60
     ///   - action: refresh action
-    public func spr_setTextAutoFooter(loadingText: String = Default.loadingText,
-                                      height: CGFloat = Default.short,
+    public func spr_setTextAutoFooter(loadingText: String = DefaultSPTR.loadingText,
+                                      height: CGFloat = DefaultSPTR.short,
                                       action: @escaping () -> Void) {
         let textItem = TextItem(loadingText: loadingText)
         spr_refreshFooter = TextFooter(textItem: textItem, height: height, isAuto: true, action: action)
     }
-
+    
     /// Custom header
     /// Inherit from RefreshView
     /// Update the presentation in 'didUpdateState(_:)' and 'didUpdateProgress(_:)' methods
@@ -150,7 +150,7 @@ public extension UIScrollView {
     public func spr_setCustomHeader(headerView: RefreshView) {
         spr_refreshHeader = headerView
     }
-
+    
     /// Custom footer
     /// Inherit from RefreshView
     /// Update the presentation in 'didUpdateState(_:)' and 'didUpdateProgress(_:)' methods
@@ -159,18 +159,18 @@ public extension UIScrollView {
     public func spr_setCustomFooter(footerView: RefreshView) {
         spr_refreshFooter = footerView
     }
-
+    
     /// begin refreshing with header
     public func spr_beginRefreshing() {
         spr_refreshHeader?.beginRefreshing()
     }
-
+    
     /// end refreshing with both header and footer
     public func spr_endRefreshing() {
         spr_refreshHeader?.endRefreshing()
         spr_refreshFooter?.endRefreshing()
     }
-
+    
     /// end refreshing with footer and remove it
     ///
     /// - Parameter message: show a label with message below the content, default is nil
@@ -179,7 +179,7 @@ public extension UIScrollView {
             guard let strongSelf = self else { return }
             strongSelf.spr_refreshFooter?.removeFromSuperview()
             strongSelf.spr_refreshFooter = nil
-
+            
             let label = UILabel()
             label.font = UIFont.systemFont(ofSize: 14)
             label.textColor = UIColor.black.withAlphaComponent(0.8)
@@ -193,14 +193,14 @@ public extension UIScrollView {
 
 // MARK: default values
 
-struct Default {
-    static let color: UIColor = UIColor.black.withAlphaComponent(0.8)
-    static let font: UIFont = UIFont.systemFont(ofSize: 14)
-    static let loadingText = "Loading..."
-    static let pullingText = "Pull down to refresh"
-    static let releaseText = "Release to refresh"
-    static let pullingFooterText = "Pull up to load more"
-    static let releaseFooterText = "Release to load more"
-    static let high: CGFloat = 120
-    static let short: CGFloat = 60
+public struct DefaultSPTR {
+    public static let color: UIColor = UIColor.black.withAlphaComponent(0.8)
+    public static let font: UIFont = UIFont.systemFont(ofSize: 14)
+    public static let loadingText = "Loading..."
+    public static let pullingText = "Pull down to refresh"
+    public static let releaseText = "Release to refresh"
+    public static let pullingFooterText = "Pull up to load more"
+    public static let releaseFooterText = "Release to load more"
+    public static let high: CGFloat = 120
+    public static let short: CGFloat = 60
 }
